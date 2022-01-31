a7ecc28bbbfa83dfed1e254bd80628ec2f3da982
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-01-31 02:22:25 +0100

    Build.

---
+++ build/precache-manifest.5bb59fed5aee28ece7d394c4340510f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c0f73f974c789761286001dc09670d",
+    "url": "/plotter/index.html"
+  },
+  {
+    "revision": "15518291cce0197cc79d",
+    "url": "/plotter/static/css/main.0b618960.chunk.css"
+  },
+  {
+    "revision": "34ea8b0ea154f123b6ac",
+    "url": "/plotter/static/js/2.205a2081.chunk.js"
+  },
+  {
+    "revision": "4432e1afa17734c4f6978af598322c56",
+    "url": "/plotter/static/js/2.205a2081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15518291cce0197cc79d",
+    "url": "/plotter/static/js/main.28e93cc4.chunk.js"
+  },
+  {
+    "revision": "8d0d08e957c2f27af4da",
+    "url": "/plotter/static/js/runtime-main.a90c81b6.js"
+  },
+  {
+    "revision": "d8cabf92a61a2f02f64148951ece74f6",
+    "url": "/plotter/static/media/moodboard.d8cabf92.png"
+  },
+  {
+    "revision": "dba121e1cf602b7ea47671b4945fc42b",
+    "url": "/plotter/static/media/tied-scroll.dba121e1.png"
+  }
+]);
