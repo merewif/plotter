5d0557797f1c41cb096f293288edc40e4670b8eb
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-02-12 09:13:07 +0100

    Removing empty object from localstorage.

--- src/Characters.js
+++ src/Characters.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CharacterOptions from "./components/character/CharacterOptions";
 
 const Characters = () => {
+  useEffect(() => {
+    if (localStorage.getItem("characters") === "{}") {
+      localStorage.removeItem("characters");
+    }
+  }, []);
+
   return (
     <div id="character-module" className="module">
       <CharacterOptions />
